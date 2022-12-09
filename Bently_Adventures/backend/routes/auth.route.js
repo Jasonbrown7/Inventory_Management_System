39bd3bcba010457b2b3cddbc718bffaca55913ab
jonathanchavarria <jchavarria@nevada.unr.edu>
2022-12-09 00:20:52 -0800

    removed debug comments in auth code

--- Bently_Adventures/backend/routes/auth.route.js
+++ Bently_Adventures/backend/routes/auth.route.js
@@ -1,76 +1,3 @@
-// const express = require('express')
-// const passport = require("passport");
-// const LocalStrategy = require("passport-local");
-// let User = require('../models/User')
-// const authRoute = express.Router()
-
-
-// passport.use(
-//   new LocalStrategy(async function verify(username, password, cb) {
-//     try {
-//       const foundUser = await User.findOne({username: username});
-//       if (!foundUser) {
-//         return cb(null, false, { message: "Incorrect username or password." });
-//       }
-//       foundUser.validatePassword(password, cb);
-//     } catch (err) {
-//       return cb(err);
-      
-//     }
-//   })
-// );
-
-// passport.serializeUser(function (user, cb) {
-//   console.log(user);
-//   process.nextTick(function () {
-//     cb(null, { id: user._id, username: user.username });
-//   });
-// });
-
-// passport.deserializeUser(function (user, cb) {
-//   process.nextTick(function () {
-//     return cb(null, user);
-//   });
-// });
-
-// authRoute.route('/login/fail').get((req, res, next) => {
-//   //console.log(req.session.user);
-//   res.status(400).send('LOGIN FAIL.');
-// })
-
-// authRoute.route('/login/success').get((req, res, next) => {
-//   //console.log(req.session);
-//   res.status(200).send('LOGIN SUCCESSFUL.');
-// })
-
-
-// authRoute.post(
-//   "/login/password",
-//   passport.authenticate("local", {
-//     successRedirect: "/login/success",
-//     failureRedirect: "/login/fail",
-//   }),
-// );
-
-// const authMiddleware = (req, res, next) => {
-//   //console.log(req);
-//   if (!req.isAuthenticated()) {
-//     res.status(401).send('You are not authenticated')
-//   } else {
-//     return next()
-//   }
-// }
-// authRoute.get("/user", authMiddleware, (req, res) => {
-  
-//   //console.log(req.session.passport);
-  
-
-//   //console.log([user, req.session])
-
-//   //res.send({ user: user })
-// })
-
-// module.exports = authRoute;
 const { Router } = require("express");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -105,13 +32,12 @@ passport.deserializeUser(function (user, cb) {
 });
 
 router.route('/login/fail').get((req, res, next) => {
-  //console.log(req.session.user);
+
   res.status(400).send('LOGIN FAIL.');
 })
 
 router.route('/login/success').get((req, res, next) => {
-  //console.log(req.session);
-  //console.log("insucess",req);
+
   res.status(200).send('LOGIN SUCCESSFUL.');
 })
 
@@ -131,7 +57,7 @@ router.post('/logout', function(req, res, next) {
 });
 
 const authMiddleware = (req, res, next) => {
-  //console.log(req);
+
   if (!req.isAuthenticated()) {
     res.status(401).send('You are not authenticated')
   } else {
--- Bently_Adventures/src/components/LoginComponent.vue
+++ Bently_Adventures/src/components/LoginComponent.vue
@@ -35,42 +35,6 @@
   </body>
   </template>
 
-<!-- <template>
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <h3 class="text-center">Login</h3>
-      <form @submit.prevent="handleSubmitForm">
-        <div class="form-group">
-          <label>Username</label>
-          <input
-            type="text"
-            class="form-control"
-            v-model="user.username"
-            required
-          />
-        </div>
-        
-        <div class="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            class="form-control"
-            v-model="user.password"
-            required
-          />
-        </div>
-
-
-        <div class="form-group">
-          <button class="btn btn-danger btn-block" ref="loginbutton">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-
-  
-</template> -->
-
 <style>
 
 .heroimage {
@@ -120,33 +84,5 @@ export default {
     },
   },
 };
-
-
-  
-//   mounted() {
-//     const logInButton = this.$refs.loginbutton;
-// const loginForm = document.forms[0];
-// console.log(logInButton);
-// async function logIn(event) {
-//   event.preventDefault();
-//   const username = loginForm[0].value;
-//   const password = loginForm[1].value;
-//   const response = await fetch("/api/create-user", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ username, password }),
-//   });
-//   if (response.redirected) window.location.replace(response.url);
-// }
-// if(logInButton){
-//   logInButton.addEventListener("click", logIn);
-// }
-//   }
-
-//};
-
-
 
 </script>
