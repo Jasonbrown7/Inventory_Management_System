e4039c787f54731929752ab2f9a8777046b79a48
Jasonbrown7 <80074205+Jasonbrown7@users.noreply.github.com>
2023-02-22 12:53:03 -0800

    create MyReservations page

---
+++ bently_rebuild/src/views/MyReservationsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+      <MyReservationsComponent/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import MyReservationsComponent from '@/components/MyReservationsComponent.vue'
+  
+  export default {
+    name: 'MyReservationscomponent',
+    components: {
+        MyReservationsComponent
+    }
+  }
+  </script>
+  
