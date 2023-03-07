583c2d6dbded1307533b4fbbdeae0a9038334183
Chandler Martin <cmartin990@gmail.com>
2023-03-06 23:38:47 -0800

    Refactor my-reservations component

--- bently_rebuild/src/components/MyReservationsComponent.vue
+++ bently_rebuild/src/components/MyReservationsComponent.vue
@@ -4,83 +4,30 @@
       <v-main color="white">
         <v-container>
           <v-row>
-            <v-col cols="2">
-              <v-toolbar color="white" elevation="0">
-              </v-toolbar>  
-              <v-sheet rounded="lg" style="border: black">
-                <v-list rounded="lg" class="grey lighten-3">
-                  <v-subheader>Filter by</v-subheader>
-                  <v-list-item
-                    link
-                    color="grey-lighten-4"
-                    :class="{'grey lighten-3': !filterCurrentlyOpen, 'grey lighten-1': filterCurrentlyOpen}"
-                    @click="filterCurrentlyOpen = !filterCurrentlyOpen"
-                  >
-                    <v-list-item-title>Currently Open</v-list-item-title>
-                  </v-list-item>
-                  <v-divider class="ma-3"></v-divider>
-                  <v-subheader>Sort by</v-subheader>
-                  <v-list-item
-                    link
-                    color="grey-lighten-4"
-                    :class="{ 'grey lighten-3': selected !== 'newest', 'grey lighten-1': selected === 'newest' }"
-                    @click="select('newest')"
-                  >
-                    <v-list-item-title>Newest</v-list-item-title>
-                  </v-list-item>
-                  <v-list-item
-                    link
-                    color="grey-lighten-4"
-                    :class="{ 'grey lighten-3': selected !== 'oldest', 'grey lighten-1': selected === 'oldest' }"
-                    @click="select('oldest')"
-                  >
-                    <v-list-item-title>Oldest</v-list-item-title>
-                  </v-list-item>
-                  <v-divider class="ma-3"></v-divider>
-                  <v-subheader class="justify-left">Reports</v-subheader>
-                  <v-btn color="primary" outlined @click="exportCsv" class="mt-1 mb-2">Export CSV</v-btn>
-                  <v-divider class="ma-3"></v-divider>
-                  <v-list-item
-                    link
-                    color="grey-lighten-4"
-                    @click="reloadPage()"
-                  >
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-sheet>
-            </v-col>
-  
             <v-col>
-              <v-toolbar color="white" elevation="0">
-                <v-toolbar-title style="font-size: 30px;">Upcoming Reservations</v-toolbar-title>
-                <v-spacer></v-spacer>
-              </v-toolbar>  
-              <v-header
-                v-if="filteredUpcomingReservations.length === 0"
-                >You have no upcoming reservations.</v-header>
-              <v-simple-table
-                v-else>
-                  <thead>
-                    <tr>
-                      <th class="text-left">Start Date</th>
-                      <th class="text-left">End Date</th>
-                      <th class="text-left">User</th>
-                      <th class="text-left">Item</th>
-                      <th class="text-left">Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="reservation in filteredUpcomingReservations" :key="reservation.id">
-                      <td>{{reservation.startDate | toDateString}}</td>
-                      <td>{{reservation.endDate | toDateString}}</td>
-                      <td>{{reservation.user | displayUserFromId(Users) }}</td>
-                      <td>{{reservation.item | displayItemFromId(Items) }}</td>
-                      <td>
-                        
-                        <v-btn v-if="!isItemCheckedOut(reservation.item)"
+              <v-toolbar color="white" elevation="0" class="mt-10">
+                    <v-toolbar-title style="font-size: 30px;">Current Reservations</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                </v-toolbar>  
+
+                <v-simple-table>
+                    <thead>
+                        <tr>
+                        <th class="text-left">Start Date</th>
+                        <th class="text-left">End Date</th>
+                        <th class="text-left">User</th>
+                        <th class="text-left">Item</th>
+                        <th class="text-left">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="reservation in filteredCurrentReservations" :key="reservation.id">
+                        <td>{{reservation.startDate | toDateString}}</td>
+                        <td>{{reservation.endDate | toDateString}}</td>
+                        <td>{{reservation.user | displayUserFromId(Users) }}</td>
+                        <td>{{reservation.item | displayItemFromId(Items) }}</td>
+                        <td>
+                          <v-btn v-if="!isItemCheckedOut(reservation.item)"
                           class="mr-md-1"
                           color="primary"
                           small
@@ -107,6 +54,45 @@
                         >
                             Cancel
                         </v-btn>
+                        </td>
+                        </tr>
+                    </tbody>
+                </v-simple-table>
+
+              <v-toolbar color="white" elevation="0">
+                <v-toolbar-title style="font-size: 30px;">Upcoming Reservations</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>  
+              <v-header
+                v-if="filteredUpcomingReservations.length === 0"
+                >You have no upcoming reservations.</v-header>
+              <v-simple-table
+                v-else>
+                  <thead>
+                    <tr>
+                      <th class="text-left">Start Date</th>
+                      <th class="text-left">End Date</th>
+                      <th class="text-left">User</th>
+                      <th class="text-left">Item</th>
+                      <th class="text-left">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="reservation in filteredUpcomingReservations" :key="reservation.id">
+                      <td>{{reservation.startDate | toDateString}}</td>
+                      <td>{{reservation.endDate | toDateString}}</td>
+                      <td>{{reservation.user | displayUserFromId(Users) }}</td>
+                      <td>{{reservation.item | displayItemFromId(Items) }}</td>
+                      <td>
+                        <v-btn
+                            class="mr-md-1"
+                            color="primary"
+                            outlined
+                            small
+                            
+                            >
+                                View Item
+                            </v-btn>
                       </td>
                     </tr>
                   </tbody>
@@ -146,7 +132,6 @@
                         </tr>
                     </tbody>
                 </v-simple-table>
-                
             </v-col>
           </v-row>
         </v-container>
@@ -239,26 +224,20 @@ export default {
       .catch((error) => {
         console.log(error);
       });
+    
       
     
   },
   computed: {
     filteredUpcomingReservations() {
-      if (this.filterCurrentlyOpen === true) {
-        return this.Reservations.filter(reservation => {
-          const startDate = new Date(reservation.startDate);
-          const endDate = new Date(reservation.endDate);
-          const today = new Date();
-          return startDate <= today && today <= endDate;
-        });
-      } else {
+    
         return this.Reservations.filter(reservation => {
             const startDate = new Date(reservation.startDate);
             const today = new Date();
             return startDate >= today;
         });
-      }
-    },
+      },
+    
     filteredPastReservations() {
         return this.Reservations.filter(reservation => {
             const startDate = new Date(reservation.startDate);
@@ -266,8 +245,18 @@ export default {
             return startDate < today;
         });
     },
-
+    filteredCurrentReservations() {
+      
+        return this.Reservations.filter(reservation => {
+          const startDate = new Date(reservation.startDate);
+          const endDate = new Date(reservation.endDate);
+          const today = new Date();
+          return startDate <= today && today <= endDate;
+        });
+    },
   },
+
+  
   methods: {
     isItemCheckedOut(item_id){
       console.log("ITEM ID", item_id);
@@ -371,6 +360,7 @@ export default {
       const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
       FileSaver.saveAs(blob, "reservations.csv");
     },
+    
   },
 };
 </script>
